492fe2c7a8e427fcda661672ec6a1f9d0df2f282
Soh Thiam Hing <daffodilistic@gmail.com>
2021-04-20 18:00:08 +0800

    Rename data JSON for MRT to mrt.json so as to avoid confusion. Moved MRT generation code into function getMrtStations().

--- generate.mjs
+++ generate.mjs
@@ -1,28 +1,38 @@
 import fs from "fs";
 import lodash from "lodash";
 
-fs.readFile("data/source/mrt_stations.json", "utf8", (err, data) => {
-  if (err) {
-    return console.log(err);
-  }
-
-  const wordlist = JSON.parse(data);
-  let outData = [];
-
-  for (const obj of wordlist) {
-    let word = obj["Station Name"];
-    // To handle "one-north" case
-    if (word.includes('-')) {
-      word = word.replace('-', ' ');
+function getMrtStations() {
+  fs.readFile("data/source/mrt_stations.json", "utf8", (err, data) => {
+    if (err) {
+      return console.log(err);
     }
 
-    word = lodash.startCase(lodash.toLower(word));
+    const wordlist = JSON.parse(data);
+    let outData = [];
 
-    outData[word] = true;
-  }
+    for (const obj of wordlist) {
+      let word = obj["Station Name"];
+      // To handle "one-north" case
+      if (word.includes('-')) {
+        word = word.replace('-', ' ');
+      }
+
+      word = lodash.startCase(lodash.toLower(word));
+
+      outData[word] = true;
+    }
+
+    outData = Object.keys(outData).sort();
+    // console.log(outData);
+
+    fs.writeFile("./data/mrt.json", JSON.stringify(outData, null, 2), (_) => { });
+  });
+}
+
 
   outData = Object.keys(outData).sort();
   // console.log(outData);
 
   fs.writeFile("./data/mrt_stations.json", JSON.stringify(outData, null, 2), (_) => { });
-});
+});});getMrtStations();getMrtStations();
+getMammals();
